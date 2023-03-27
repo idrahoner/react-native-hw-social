9f44b49dc78e5a9ca66ed1d64e97118c3e4c1236
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-03-27 09:11:14 +0300

    Add icons to project

--- Screens/PostsScreen/index.js
+++ Screens/PostsScreen/index.js
@@ -1,10 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 export default function PostsScreen() {
   return (
     <View style={styles.container}>
       <Text>This is PostsScreen component!</Text>
+      <TouchableOpacity>
+        <AntDesign name="arrowleft" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <AntDesign name="arrowup" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
+      <View style={{ flexDirection: 'row', gap: 16, marginVertical: 16 }}>
+        <TouchableOpacity>
+          <SimpleLineIcons name="grid" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Fontisto name="plus-a" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather name="user" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity>
+        <Feather name="trash-2" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <AntDesign name="enviromento" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <MaterialIcons name="camera-alt" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Feather name="thumbs-up" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Ionicons
+          name="chatbubble-sharp"
+          size={24}
+          color="black"
+          style={{ transform: [{ scaleX: -1 }] }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Ionicons
+          name="chatbubble-outline"
+          size={24}
+          color="black"
+          style={{ transform: [{ scaleX: -1 }] }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
