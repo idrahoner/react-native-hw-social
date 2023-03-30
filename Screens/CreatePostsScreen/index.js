4ab461bf4ab21cf261766c732b3c96e565eae0e3
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-03-30 11:47:14 +0300

    Add base layout of create post screen

--- Screens/CreatePostsScreen/index.js
+++ Screens/CreatePostsScreen/index.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { MaterialIcons, Feather, AntDesign } from '@expo/vector-icons';
+import PrimaryButton from '../../components/PrimaryButton';
 
 export default function CreatePostsScreen() {
+  const [permision, requestPermision] = Camera.useCameraPermissions();
+  if (!permision) {
+    return null;
+  }
+  if (!permision.granted) {
+    return (
+      <TouchableOpacity onPress={requestPermision}>
+        <Text>Set permision</Text>
+      </TouchableOpacity>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>This is CreatePostsScreen component!</Text>
+      <View style={styles.contentContainer}>
+        <View style={styles.cameraContainer}>
+          <Camera style={styles.camera} type={CameraType.back}>
+            <TouchableOpacity style={styles.snapButton}>
+              <MaterialIcons name="camera-alt" size={24} color="black" />
+            </TouchableOpacity>
+          </Camera>
+          <TouchableOpacity style={styles.loadButton}>
+            <Text style={styles.loadTitle}>Load photo</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.imageDataForm}>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="Title" />
+          </View>
+          <View style={styles.inputContainer}>
+            <AntDesign name="enviromento" size={24} color="black" />
+            <TextInput placeholder="Location" />
+          </View>
+        </View>
+        <PrimaryButton title="publish" disabled />
+      </View>
+      <TouchableOpacity>
+        <Feather name="trash-2" size={24} color="black" />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +57,34 @@ export default function CreatePostsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 32,
+    backgroundColor: '#fff',
+  },
+  contentContainer: { width: '100%', gap: 32 },
+  camera: {
     justifyContent: 'center',
+    alignItems: 'center',
+    height: 240,
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  snapButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: 'white',
+  },
+  loadButton: { marginTop: 8 },
+  loadTitle: { fontFamily: 'Roboto-regular', fontSize: 16, color: '#BDBDBD' },
+  imageDataForm: { gap: 16 },
+  inputContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 2,
+    borderBottomColor: 'grey',
   },
 });
--- components/PrimaryButton/index.js
+++ components/PrimaryButton/index.js
@@ -1,13 +1,21 @@
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function PrimaryButton({ title, style, onPress }) {
+export default function PrimaryButton({ title, style, onPress, disabled }) {
   return (
     <TouchableOpacity
-      style={{ ...styles.button, ...style }}
+      style={{
+        ...styles.button,
+        backgroundColor: disabled ? '#F6F6F6' : '#FF6C00',
+        ...style,
+      }}
       onPress={onPress}
       activeOpacity={0.6}
     >
-      <Text style={styles.title}>{title}</Text>
+      <Text
+        style={{ ...styles.title, color: disabled ? '#BDBDBD' : '#FFFFFF' }}
+      >
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
@@ -19,11 +27,9 @@ const styles = StyleSheet.create({
     height: 50,
     padding: 16,
     borderRadius: 100,
-    backgroundColor: '#FF6C00',
   },
   title: {
     fontSize: 16,
     fontFamily: 'Roboto-regular',
-    color: '#FFFFFF',
   },
 });
